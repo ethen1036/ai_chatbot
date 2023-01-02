c1026fc1cab3dc026796c53de048696b95ba34d1
Shubham Kumar <sraj94710@gmail.com>
2023-01-02 07:00:49 +0530

    Live server URL updated

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatbot-5avm.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
